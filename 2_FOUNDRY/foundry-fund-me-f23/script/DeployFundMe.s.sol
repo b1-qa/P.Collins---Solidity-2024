78ed2b3044c63afe3eaec7d00f4a065af6db295e
b1-qa <bryce@w3bchain.co>
2025-03-17 01:03:16 +0700

    mock chainlink supported

--- 2_FOUNDRY/foundry-fund-me-f23/script/DeployFundMe.s.sol
+++ 2_FOUNDRY/foundry-fund-me-f23/script/DeployFundMe.s.sol
@@ -4,13 +4,18 @@ pragma solidity ^0.8.21;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     FundMe fundMe;
 
     function run() external returns (FundMe) {
+        //we put it before startbroadcast so it doesn't cost gas, it's a fake tx
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+        //now we pay gas as it's real tx
         vm.startBroadcast();
-        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
